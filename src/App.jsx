db1f217db1937111d8b513786a26b97327c8a8de
DarioCostaAlves <dario.c.alves22@gmail.com>
2023-11-13 22:48:08 +0000

    structing folders and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import { useState } from "react";
+//CSS
 import "./App.css";
+//COMPONENTS
 import Button from "./components/Button";
-import { user, users } from "./constants";
-
-const nome = "Dário Alves";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
-      <h1 className="shreck">
-        Veterinário principal: <br /> {user.name}
-      </h1>
-      <Button />
-      <div>
-        <h3>Todos os veterinários: </h3>
-
-        {users.map((user) => (
-          <p key={user.email}>{user.name}</p>
-        ))}
-      </div>
+      <Navbar />
+      <Hero />
+      <Footer />
     </div>
   );
 };
